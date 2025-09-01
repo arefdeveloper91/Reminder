0c2577d37f54a9ac08d2fcc9e656bfd6bac31ab7
aref <devfera14@gmail.com>
2025-09-01 14:24:01 -0300

    Initial Commit

---
+++ Reminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reminder
+//
+//  Created by arefdeveloper on 01/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by arefdeveloper on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
